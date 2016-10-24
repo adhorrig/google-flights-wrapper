4efcac14a1f093f316d81f4576db761c51971280
Adam <adhorrig@gmail.com>
2016-10-24 18:05:00 +0100

    Export as module

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-var request = require('request');
-var data = require('./data.json');
-var key = '';
-var endPoint = "https://www.googleapis.com/qpxExpress/v1/trips/search?key="+key;
+var qpx = modules.export{
+  var request = require('request');
+  var data = require('./data.json');
+  var key = '';
+  var endPoint = "https://www.googleapis.com/qpxExpress/v1/trips/search?key="+key;
 
-function api(adultCount, maxPrice, solutions, origin, destination, date){
+  api: function(adultCount, maxPrice, solutions, origin, destination, date){
 
-  data.request.passengers.adultCount = adultCount;
-  data.request.maxPrice = maxPrice;
-  data.request.solutions = solutions;
-  data.request.slice[0].origin = origin;
-  data.request.slice[0].destination = destination;
-  data.request.slice[0].date = date;
+    data.request.passengers.adultCount = adultCount;
+    data.request.maxPrice = maxPrice;
+    data.request.solutions = solutions;
+    data.request.slice[0].origin = origin;
+    data.request.slice[0].destination = destination;
+    data.request.slice[0].date = date;
 
 
-  request({method: "post",  url: endPoint,  body: data,  json: true}, function(err, resp, body){
+    request({method: "post",  url: endPoint,  body: data,  json: true}, function(err, resp, body){
 
-    var info = [];
-    var jsonObject = {};
-    var airline;
-    var price;
+      var info = [];
+      var jsonObject = {};
+      var airline;
+      var price;
 
-    if (body.error){
-      return console.error(body.error);
-    } else {
-      var price= body.trips.tripOption[0].saleTotal;
-      if(solutions >= 2){
-        for(i=0; i < body.trips.tripOption.length; i++){
-          airline = body.trips.tripOption[i].slice[0].segment[0].flight.carrier
-          price = body.trips.tripOption[i].saleTotal
-          jsonObject = {"airline": airline , "price": price};
-          info.push(jsonObject);
-        }
-        return info;
+      if (body.error){
+        return console.error(body.error);
       } else {
-        airline = body.trips.tripOption[0].slice[0].segment[0].flight.carrier
-        price = body.trips.tripOption[0].saleTotal
-        jsonObject = {"airline": airline, "price":price}
-        info.push(jsonObject);
-        return info;
+        var price= body.trips.tripOption[0].saleTotal;
+        if(solutions >= 2){
+          for(i=0; i < body.trips.tripOption.length; i++){
+            airline = body.trips.tripOption[i].slice[0].segment[0].flight.carrier
+            price = body.trips.tripOption[i].saleTotal
+            jsonObject = {"airline": airline , "price": price};
+            info.push(jsonObject);
+          }
+          return info;
+        } else {
+          airline = body.trips.tripOption[0].slice[0].segment[0].flight.carrier
+          price = body.trips.tripOption[0].saleTotal
+          jsonObject = {"airline": airline, "price":price}
+          info.push(jsonObject);
+          return info;
+        }
       }
-    }
-  });
+    });
+  }
 }
